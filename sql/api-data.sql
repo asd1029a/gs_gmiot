592b236775dc5795808b78b1ff88f811457a48b4
hansik.shin <hansik.shin@danusys.com>
2021-11-11 14:34:35 +0900

    컬럼 추가

--- sql/api-data.sql
+++ sql/api-data.sql
@@ -1,15 +1,20 @@
 INSERT INTO public.api
-(id, call_url, method_type, request_body_type, response_body_type, api_type, target_url, target_path, content_type, auth_info)
-VALUES(1,'/aepel/findDeviceList', 'POST', 'OBJECT', 'MULTISET', 'REST', 'http://aepel.co.kr', '/api/deviceInfoList.json', 'application/json', '');
+(call_url, method_type, request_body_type, response_body_type, api_type, target_url, target_path, content_type, service_nm, service_prefix, auth_info)
+VALUES('/aepel/findDeviceList', 'POST', 'OBJECT', 'MULTISET', 'REST', 'http://aepel.co.kr', '/api/deviceInfoList.json', 'application/json', '','','');
+
 INSERT INTO public.api
-(id, call_url, method_type, request_body_type, response_body_type, api_type, target_url, target_path, content_type, auth_info)
-VALUES(2, '/aepel/whInfoList', 'POST', 'OBJECT', 'MULTISET', 'REST', 'http://aepel.co.kr', '/api/whInfoList.json', 'application/json', '');
+(id, call_url, method_type, request_body_type, response_body_type, api_type, target_url, target_path, content_type, service_nm, service_prefix, auth_info)
+VALUES(2, '/aepel/whInfoList', 'POST', 'OBJECT', 'MULTISET', 'REST', 'http://aepel.co.kr', '/api/whInfoList.json', 'application/json', '','','');
+
+
 INSERT INTO public.api
-(id, call_url, method_type, request_body_type, response_body_type, api_type, target_url, target_path, content_type, auth_info)
-VALUES(3, '/aepel/login', 'POST', 'OBJECT', 'MULTISET', 'REST', 'http://aepel.co.kr', '/api/login.sso', 'application/json', '');
+(id, call_url, method_type, request_body_type, response_body_type, api_type, target_url, target_path, content_type, service_nm, service_prefix, auth_info)
+VALUES(3, '/aepel/login', 'POST', 'OBJECT', 'MULTISET', 'REST', 'http://aepel.co.kr', '/api/login.sso', 'application/json', '','','');
+
 INSERT INTO public.api
-(id, call_url, method_type, request_body_type, response_body_type, api_type, target_url, target_path, content_type, auth_info)
-VALUES(4, '/epkor/findDeviceList', 'POST', 'OBJECT', 'OBJECT', 'SOAP', 'http://localhost:9091/ws', 'http://www.danusys.com/springsoap/gen', '', '');
+(id, call_url, method_type, request_body_type, response_body_type, api_type, target_url, target_path, content_type, service_nm, service_prefix, auth_info)
+VALUES(4, '/epkor/findDeviceList', 'POST', 'OBJECT', 'MULTISET', 'SOAP', 'http://localhost:9091/ws', 'http://www.danusys.com/springsoap/gen', '', 'getCountryRequest','gs','');
+
 
 INSERT INTO public.api_param (id, seq, field_nm, field_map_nm, data_type, required, param_type, value, description, api_id) values
 (1, 1, 'auth_key', 'key','STRING','true','REQUEST','','인터페이스간 약속된 키', 1)
