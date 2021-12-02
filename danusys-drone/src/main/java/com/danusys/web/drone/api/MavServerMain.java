0989201ea2be4c0490f5ea14ebf09c9eb342e39b
hansik.shin <hansik.shin@danusys.com>
2021-12-02 19:40:31 +0900

    마브링크 이륙 & 미션 테스트 진행중

---
+++ danusys-drone/src/main/java/com/danusys/web/drone/api/MavServerMain.java
@@ -0,0 +1,16 @@
+package com.danusys.web.drone.api;
+
+/**
+ * Project : danusys-webservice-parent
+ * Created by IntelliJ IDEA
+ * Developer : kai
+ * Date : 2021/11/20
+ * Time : 1:25 오전
+ */
+
+
+public class MavServerMain {
+    public static void main(String[] args) {
+//        MAVLinkConnection.createConnection();
+    }
+}
