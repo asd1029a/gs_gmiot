67203f845827c66828be0154755175ffbf561ee1
DESKTOP-4QDROAE\hawool <@hawool>
2021-12-16 15:51:06 +0900

    조회 및 관리 페이지: 1차 레이아웃 분리작업

--- danusys-platform/src/main/java/com/danusys/web/platform/config/SecurityConfig.java
+++ danusys-platform/src/main/java/com/danusys/web/platform/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 						, "/ui/**"
                         , "/mntr/**"
 						, "/pages/**"
+						, "/pages/inqry/**"
 						, "/webjars/**" ).permitAll()
 				.anyRequest().authenticated()
 				.and()
