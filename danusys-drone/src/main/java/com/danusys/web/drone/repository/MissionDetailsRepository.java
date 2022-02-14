d68b300f51c5416b40933ea3837380d87e13c51e
eomtaehyeok <xogur1212@danusys.com>
2022-02-14 11:16:47 +0900

    missiondetails  index 기준 오름차순 정렬

--- danusys-drone/src/main/java/com/danusys/web/drone/repository/MissionDetailsRepository.java
+++ danusys-drone/src/main/java/com/danusys/web/drone/repository/MissionDetailsRepository.java
@@ -2,6 +2,7 @@ package com.danusys.web.drone.repository;
 
 import com.danusys.web.drone.model.Mission;
 import com.danusys.web.drone.model.MissionDetails;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -11,6 +12,8 @@ public interface MissionDetailsRepository extends CrudRepository<MissionDetails,
     List<MissionDetails> findAllByName(String name);
 
     MissionDetails findByNameAndMission(String name, Mission mission);
+
+    List<MissionDetails> findAllByMission(Mission mission, Sort sort);
 
     List<MissionDetails> findAllByMission(Mission mission);
 
