0732a742598ebcb1684d01884fcf7283ea64ddf9
kjm <kimjm9347@danusys.com>
2022-03-21 16:46:20 +0900

    danusys-commons-tcp-server 추가

--- danusys-commons-tcp-server/src/main/java/com/danusys/tcp/server/config/ServerSocket.java
+++ danusys-commons-tcp-server/src/main/java/com/danusys/tcp/server/config/ServerSocket.java
@@ -16,7 +16,7 @@ import java.net.InetSocketAddress;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class TCPServer {
+public class ServerSocket {
 
     private final ServerBootstrap serverBootstrap;
 
@@ -26,8 +26,12 @@ public class TCPServer {
 
     public void start()  {
         try {
+            // ChannelFuture: I/O operation의 결과나 상태를 제공하는 객체
+            // 지정한 host, port로 소켓을 바인딩하고 incoming connections을 받도록 준비함
             ChannelFuture serverChannelFuture = serverBootstrap.bind(tcpPort).sync();
-            log.info("Server Start : port {}", tcpPort.getPort());
+            log.info("Server port : {}", tcpPort.getPort());
+
+            // 서버 소켓이 닫힐 때까지 기다림
             serverChannel = serverChannelFuture.channel().closeFuture().sync().channel();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -40,15 +44,5 @@ public class TCPServer {
             serverChannel.close();
             serverChannel.parent().close();
         }
-    }
-    @SuppressWarnings({"Convert2Lambda", "java:S1604"})
-    @Bean
-    public ApplicationListener<ApplicationReadyEvent> readyEventApplicationListener() {
-        return new ApplicationListener<ApplicationReadyEvent>() {
-            @Override
-            public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-                start();
-            }
-        };
     }
 }
--- danusys-commons-tcp-server/src/main/java/com/danusys/tcp/server/handler/ServerHandler.java
+++ danusys-commons-tcp-server/src/main/java/com/danusys/tcp/server/handler/ServerHandler.java
@@ -16,22 +16,24 @@ import java.nio.charset.Charset;
 @ChannelHandler.Sharable
 public class ServerHandler extends ChannelInboundHandlerAdapter {
 
-    private static final Charset CHARSET = Charset.forName("UTF-8");
+    private static final Charset CHARSET = Charset.forName("EUC-KR");
     private final MessageCallback writer;
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf inBuffer = (ByteBuf) msg;
         String received = inBuffer.toString(CHARSET);
-        if(writer == null){
-            System.out.println("Server received : " + received);
-            ctx.write(Unpooled.copiedBuffer("echo : "+received, CHARSET));
-        } else {
-            writer.read(received);
-            String arg = writer.write(received);
-            ctx.write(Unpooled.copiedBuffer(arg.toCharArray(), CHARSET));
-        }
+        writer.read(received);
+        String arg = writer.write(received);
+        ctx.write(Unpooled.copiedBuffer(arg.toCharArray(), CHARSET));
     }
+    // 클라이언트와 연결되어 트래픽을 생성할 준비가 되었을 때 호출되는 메소드
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        String remoteAddress = ctx.channel().remoteAddress().toString();
+        log.info("Remote Address: " + remoteAddress);
+    }
+
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx){
         ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);
