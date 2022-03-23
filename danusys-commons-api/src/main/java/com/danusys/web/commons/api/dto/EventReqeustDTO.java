f5ecdc1a9200ff7b941c7780dd6110d8c6589ab0
liu <ippo910118@danusys.com>
2022-03-23 12:12:20 +0900

    이벤트 station_seq 추가

--- danusys-commons-api/src/main/java/com/danusys/web/commons/api/dto/EventReqeustDTO.java
+++ danusys-commons-api/src/main/java/com/danusys/web/commons/api/dto/EventReqeustDTO.java
@@ -3,7 +3,10 @@ package com.danusys.web.commons.api.dto;
 import com.danusys.web.commons.api.model.Event;
 import lombok.Getter;
 import lombok.Setter;
+import org.codehaus.commons.nullanalysis.NotNull;
 import org.springframework.data.jpa.repository.Query;
+
+import javax.annotation.Nullable;
 
 /**
  * Project : danusys-webservice-parent
@@ -16,9 +19,20 @@ import org.springframework.data.jpa.repository.Query;
 @Setter
 public class EventReqeustDTO {
     private Long facilitySeq;
-    private String facilityId;
+    private Long stationSeq;
     private int eventKind;
+    private String facilityId;
     private String eventKindNm;
     private String eventGrade;
     private String eventMessage;
+
+    public Event toEntity() {
+        return Event.builder()
+                .facilitySeq(this.facilitySeq)
+                .stationSeq(this.stationSeq)
+                .eventKind(this.eventKind)
+                .eventGrade(this.eventGrade)
+                .eventMessage(this.eventMessage)
+                .build();
+    }
 }
