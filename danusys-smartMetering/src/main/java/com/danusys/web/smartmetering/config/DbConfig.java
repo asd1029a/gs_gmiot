20da9ed3cb99c8926e59f72f2a425b607d8b0674
nahyun <danunhk@danusys.com>
2022-04-12 09:59:35 +0900

    DBConfig수정완료

--- danusys-smartMetering/src/main/java/com/danusys/web/smartmetering/config/DbConfig.java
+++ danusys-smartMetering/src/main/java/com/danusys/web/smartmetering/config/DbConfig.java
@@ -6,15 +6,17 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource({"classpath:/application-local.properties"})
 public class DbConfig {
 
     private final ApplicationContext applicationContext;
@@ -23,28 +25,32 @@ public class DbConfig {
         this.applicationContext = applicationContext;
     }
 
-   @Bean(name = "dataSource")
-   public DataSource dataSource(){
-         return DataSourceBuilder
-                 .create()
-                 .driverClassName("org.mariadb.jdbc.Driver")
-                 .url("jdbc:mariadb://danuservice2.kro.kr:41036/smartMeteringTest")
-  //               .url("jdbc:mariadb://localhost:3306/smartmeteringmaria")
-                 .username("root")
-                 .password("danu1234")
-                 .build();
-//       DataSourceBuilder.create().build();
-   }
+    @Value("${spring.datasource.username}") String username;
+    @Value("${spring.datasource.password}") String password;
+    @Value("${spring.datasource.url}")  String url;
+    @Value("${spring.datasource.driver-class-name}")  String driverClass;
+    @Value("${mybatis.mapper-locations}")  String mapperLocation;
+    @Value("${mybatis.config-location}")  String configLocation;
+
+    @Bean(name = "dataSource")
+    public DataSource datasource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
 
     @Bean(name="sqlSessionFactory")
     public SqlSessionFactory sqlSessionFactoryBean(@Autowired @Qualifier("dataSource") DataSource dataSource,
                                                    ApplicationContext applicationContext)
             throws Exception {
         SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setConfigLocation(applicationContext.getResource(configLocation));
+        sessionFactoryBean.setMapperLocations(applicationContext.getResources(mapperLocation));
 
-        sessionFactoryBean.setConfigLocation(applicationContext.getResource("classpath:mapper/config/mybatis-config.xml"));
-        sessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:mapper/sql/*.xml"));
         return sessionFactoryBean.getObject();
     }
 
