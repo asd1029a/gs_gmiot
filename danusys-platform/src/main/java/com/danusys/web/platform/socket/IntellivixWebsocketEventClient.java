e0900a04d24b1521fedfd45f121c78d043c79add
yuna <riyu0706>
2022-05-20 18:28:30 +0900

    merge 누락 import

--- danusys-platform/src/main/java/com/danusys/web/platform/socket/IntellivixWebsocketEventClient.java
+++ danusys-platform/src/main/java/com/danusys/web/platform/socket/IntellivixWebsocketEventClient.java
@@ -1,6 +1,7 @@
 package com.danusys.web.platform.socket;
 
 import com.danusys.web.commons.app.RestUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
