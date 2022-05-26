15f16df11175111893633b1b407e2edb6b858a56
ndw2280 <ndw2280@danusys.com>
2022-05-26 17:23:35 +0900

    https 우회 통신 임시추가

---
+++ danusys-commons-api/src/main/java/com/danusys/web/commons/api/config/SslConfig.java
@@ -0,0 +1,51 @@
+package com.danusys.web.commons.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
+
+/**
+ * Project : danusys-webservice-parent
+ * Created by Intellij IDEA
+ * Developer : ndw85
+ * Date : 2022/05/25
+ * Time : 17:28
+ */
+@Profile("bsng")
+@Configuration
+public class SslConfig {
+    @Bean
+    public Boolean disableSSLValidation() throws Exception {
+        final SSLContext sslContext = SSLContext.getInstance("TLS");
+
+        sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] x509Certificates, String s) throws java.security.cert.CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] x509Certificates, String s) throws java.security.cert.CertificateException {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+        }}, null);
+
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+
+        return true;
+    }
+}
