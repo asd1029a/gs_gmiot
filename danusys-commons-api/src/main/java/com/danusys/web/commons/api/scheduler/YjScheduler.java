47c339a134d968bfdd367b62b832335f8af2ffb4
kjm <kimjm9347@danusys.com>
2022-07-26 15:27:46 +0900

    부산남구,영주 ip 시설물 장애 여부 update

--- danusys-commons-api/src/main/java/com/danusys/web/commons/api/scheduler/YjScheduler.java
+++ danusys-commons-api/src/main/java/com/danusys/web/commons/api/scheduler/YjScheduler.java
@@ -2,9 +2,13 @@ package com.danusys.web.commons.api.scheduler;
 
 import com.danusys.web.commons.api.dto.FacilityDataRequestDTO;
 import com.danusys.web.commons.api.model.Facility;
+import com.danusys.web.commons.api.model.FacilityActiveLog;
+import com.danusys.web.commons.api.model.FacilityOpt;
+import com.danusys.web.commons.api.repository.FacilityActiveRepository;
 import com.danusys.web.commons.api.service.FacilityOptService;
 import com.danusys.web.commons.api.service.FacilityService;
 import com.danusys.web.commons.api.util.ApiUtils;
+import com.danusys.web.commons.api.util.IpCheckedUtil;
 import com.danusys.web.commons.app.StrUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +19,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +33,9 @@ public class YjScheduler {
     private final ObjectMapper objectMapper;
     private final FacilityOptService facilityOptService;
     private final FacilityService facilityService;
+    private final FacilityActiveRepository facilityActiveRepository;
+    private static long FACILITY_SEQ = 9588L; //실서버 facility_seq
+//    private static long FACILITY_SEQ = 9722L; // 로컬서버 facility_seq
 
     /**
      * 정류장 유동인구 저장
@@ -56,5 +64,32 @@ public class YjScheduler {
                 e.printStackTrace();
             }
         });
+    }
+
+    /**
+     *
+     * 시설물 장애 이벤트 ping check
+     */
+    @Scheduled(cron = "0 0 0/1 * * *")
+    public void ipPingCheck(){
+        Long faSeq = FACILITY_SEQ;
+        List<FacilityOpt> facilityOptList = facilityOptService.findByFacilitySeq(faSeq);
+        List<Map<String, Object>> ipLists = new ArrayList<>();
+        List<FacilityActiveLog> facilityActiveLogList = new ArrayList<>();
+        FacilityActiveLog facilityActiveLog = new FacilityActiveLog();
+        facilityOptList.stream().filter(f -> f.getFacilityOptName().equals("ip"))
+                .forEach(facilityOpt -> {
+                    Map<String,Object> maps = new HashMap<>();
+                    maps.put(facilityOpt.getFacilityOptName(),facilityOpt.getFacilityOptValue());
+                    ipLists.add(maps);
+                });
+        IpCheckedUtil.ipCheckedList(ipLists);
+
+        ipLists.stream().forEach(f -> {
+            FacilityActiveLog build = facilityActiveLog.builder().facilitySeq(faSeq).
+                    facilityActiveCheck((boolean) f.get("active")).facilityActiveIp((String) f.get("ip")).build();
+            facilityActiveLogList.add(build);
+        });
+        facilityActiveRepository.saveAll(facilityActiveLogList);
     }
 }
