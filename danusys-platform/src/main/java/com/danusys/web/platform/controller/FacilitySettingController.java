aa82a8e4393df2f10177cdeeaa1003ffdbecdfac
kjm <kimjm9347@danusys.com>
2022-07-27 17:06:54 +0900

    시설물 제어 컨트롤러 변경

--- danusys-platform/src/main/java/com/danusys/web/platform/controller/FacilitySettingController.java
+++ danusys-platform/src/main/java/com/danusys/web/platform/controller/FacilitySettingController.java
@@ -1,7 +1,7 @@
 package com.danusys.web.platform.controller;
 
 import com.danusys.web.commons.api.dto.SettingDTO;
-import com.danusys.web.commons.api.scheduler.service.SchedulerService;
+import com.danusys.web.commons.api.scheduler.service.YjMqttFacility;
 import com.danusys.web.commons.api.service.FacilitySettingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +17,7 @@ import java.util.List;
 public class FacilitySettingController {
 
     private final FacilitySettingService facilitySettingService;
-    private final SchedulerService schedulerService;
+    private final YjMqttFacility yjMqttFacility;
 
     @PostMapping("/{facilitySeq}")
     public ResponseEntity<List<SettingDTO>> getAllList(@PathVariable Long facilitySeq) throws Exception {
@@ -37,7 +37,7 @@ public class FacilitySettingController {
     @PostMapping
     public ResponseEntity<?> saveList(@RequestBody List<SettingDTO> settingDTOList) throws Exception {
         facilitySettingService.save(settingDTOList);
-        schedulerService.setScheduler();
+        yjMqttFacility.setScheduler();
         return ResponseEntity.noContent().build();
     }
 
