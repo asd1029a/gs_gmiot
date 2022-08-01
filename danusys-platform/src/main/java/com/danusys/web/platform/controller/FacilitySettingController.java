49a0b3ec5a71a44730b5b838195640802db6bd0e
ndw2280 <ndw2280@danusys.com>
2022-08-01 17:53:11 +0900

    yj scheduler service 추가

--- danusys-platform/src/main/java/com/danusys/web/platform/controller/FacilitySettingController.java
+++ danusys-platform/src/main/java/com/danusys/web/platform/controller/FacilitySettingController.java
@@ -3,9 +3,11 @@ package com.danusys.web.platform.controller;
 import com.danusys.web.commons.api.dto.SettingDTO;
 import com.danusys.web.commons.api.scheduler.service.GmSchedulerService;
 import com.danusys.web.commons.api.scheduler.service.YjMqttFacility;
+import com.danusys.web.commons.api.scheduler.service.YjSchedulerService;
 import com.danusys.web.commons.api.service.FacilitySettingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,10 @@ public class FacilitySettingController {
 
     private final FacilitySettingService facilitySettingService;
     private final GmSchedulerService gmSchedulerService;
+    private final YjSchedulerService yjSchedulerService;
+
+    @Value("${danusys.area.code.sig}")
+    private String sigCode;
 
     @PostMapping("/{facilitySeq}")
     public ResponseEntity<List<SettingDTO>> getAllList(@PathVariable Long facilitySeq) throws Exception {
@@ -38,8 +44,11 @@ public class FacilitySettingController {
     @PostMapping
     public ResponseEntity<?> saveList(@RequestBody List<SettingDTO> settingDTOList) throws Exception {
         facilitySettingService.save(settingDTOList);
-        /*yjMqttFacility.setScheduler();*/
-        gmSchedulerService.setScheduler();
+        if("47210".equals(sigCode)) {
+            yjSchedulerService.setScheduler();
+        } else {
+            gmSchedulerService.setScheduler();
+        }
         return ResponseEntity.noContent().build();
     }
 
