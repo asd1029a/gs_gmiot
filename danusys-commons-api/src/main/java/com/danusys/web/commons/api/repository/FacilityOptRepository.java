965b421145470092436ef4b02e1a8378fb77eb80
liu <ippo910118@danusys.com>
2022-08-30 16:41:35 +0900

    JPQL 에러(@Param 추가)

--- danusys-commons-api/src/main/java/com/danusys/web/commons/api/repository/FacilityOptRepository.java
+++ danusys-commons-api/src/main/java/com/danusys/web/commons/api/repository/FacilityOptRepository.java
@@ -3,6 +3,7 @@ package com.danusys.web.commons.api.repository;
 import com.danusys.web.commons.api.model.FacilityOpt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +22,5 @@ public interface FacilityOptRepository extends JpaRepository<FacilityOpt, Long> 
     List<FacilityOpt> findByFacilitySeq(Long facilitySeq);
     FacilityOpt findByFacilityOptNameAndFacilityOptValue(String facilityOptName, String facilityOptValue);
     @Query(value = "SELECT * FROM t_facility_opt WHERE facility_opt_seq IN (SELECT MAX(facility_opt_seq) FROM t_facility_opt WHERE facility_seq = :facilitySeq GROUP BY facility_opt_name)", nativeQuery = true)
-    List<FacilityOpt> findByFacilitySeqLast(Long facilitySeq);
+    List<FacilityOpt> findByFacilitySeqLast(@Param("facilitySeq") Long facilitySeq);
 }
--- danusys-commons-api/src/main/java/com/danusys/web/commons/api/repository/FacilityRepository.java
+++ danusys-commons-api/src/main/java/com/danusys/web/commons/api/repository/FacilityRepository.java
@@ -4,6 +4,7 @@ import com.danusys.web.commons.api.model.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.JpaQueryMethodFactory;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -22,23 +23,23 @@ public interface FacilityRepository extends JpaRepository<Facility, Long> {
     Facility findByFacilitySeq(Long facilitySeq);
 
     @Query(value = "SELECT code_seq FROM v_facility_kind WHERE code_id = :codeId", nativeQuery = true)
-    Long findCommonCode(String codeId);
+    Long findCommonCode(@Param("codeId") String codeId);
 
     @Query(value = "SELECT code_name FROM v_facility_kind WHERE code_seq = :code_seq", nativeQuery = true)
-    String findCommonCodeName(Long code_seq);
+    String findCommonCodeName(@Param("code_seq") Long code_seq);
 
     List<Facility> findByFacilityKind(Long facilityKind);
 
     @Query(value = "SELECT fn_lonlat_to_emdcode(:longitude, :latitude)", nativeQuery = true)
-    String getEmdCode(double longitude, double latitude);
+    String getEmdCode(@Param("longitude") double longitude, @Param("latitude") double latitude);
 
     List<Facility> findByStationSeq(Long stationSeq);
 
     @Query(value = "SELECT code_seq FROM v_facility_kind WHERE code_value IN (:facilityKindValues)", nativeQuery = true)
-    List<Long> findFacilityKindList(List<String> facilityKindValues);
+    List<Long> findFacilityKindList(@Param("facilityKindValues") List<String> facilityKindValues);
 
     @Query(value = "SELECT * FROM t_facility WHERE administ_zone like :administZone% AND facility_kind IN (:facilityKind)", nativeQuery = true)
-    List<Facility> findByAdministZoneAndFacilityKindIn(String administZone, List<Long> facilityKind);
+    List<Facility> findByAdministZoneAndFacilityKindIn(@Param("administZone") String administZone, @Param("facilityKind") List<Long> facilityKind);
 
     List<Facility> findByFacilityKindAndLatitudeAndLongitude(Long facilityKind, double latitude, double longitude);
 }
