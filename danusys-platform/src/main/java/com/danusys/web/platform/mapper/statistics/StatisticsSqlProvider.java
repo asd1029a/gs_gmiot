2150f4a2ac3b08c029f61514b5df2bd817ab77f3
yuna <yuna706@danusys.com>
2022-09-02 19:47:49 +0900

    통계 facility opt 데이터 추가 3
    - facility opt 맵 차트 완료

--- danusys-platform/src/main/java/com/danusys/web/platform/mapper/statistics/StatisticsSqlProvider.java
+++ danusys-platform/src/main/java/com/danusys/web/platform/mapper/statistics/StatisticsSqlProvider.java
@@ -408,25 +408,19 @@ public class StatisticsSqlProvider {
         SQL sql = new SQL() {{
             String startDt = CommonUtil.validOneNull(paramMap, "startDt");
             String endDt = CommonUtil.validOneNull(paramMap, "endDt");
-            ArrayList<String> eventKind = CommonUtil.valiArrNull(paramMap, "eventKind");
+            String optName = CommonUtil.validOneNull(paramMap, "optName");
 
-            SELECT("v4.code_name AS name, count(*) AS value");
-            FROM("t_event t1" +
+            SELECT("v1.code_name AS name, sum(facility_opt_value::int) AS value");
+            FROM("t_facility_opt t1" +
                     "    INNER JOIN t_facility t2 ON t1.facility_seq = t2.facility_seq" +
-                    "    INNER JOIN t_station t3 ON t1.station_seq = t3.station_seq" +
-                    "    INNER JOIN v_event_kind v1 ON t1.event_kind = v1.code_seq" +
-                    "    INNER JOIN v_event_grade v2 ON t1.event_grade = v2.code_seq" +
-                    "    INNER JOIN v_event_proc_stat v3 ON t1.event_proc_stat = v3.code_seq" +
-                    "    INNER JOIN v_administ v4 ON t2.administ_zone = v4.code_value");
+                    "    INNER JOIN v_administ v1 ON t2.administ_zone = v1.code_value");
+            WHERE("facility_opt_name = '" + optName + "'");
 
             if (!startDt.equals("")) {
                 WHERE("t1.insert_dt >= to_timestamp('" + startDt + "', 'YYYY-MM-DD HH24:MI:SS')");
             }
             if (!endDt.equals("")) {
                 WHERE("t1.insert_dt <= to_timestamp('" + endDt + "', 'YYYY-MM-DD HH24:MI:SS')");
-            }
-            if (eventKind != null && !eventKind.isEmpty()) {
-                WHERE("v1.code_value" + SqlUtil.getWhereInStr(eventKind));
             }
             GROUP_BY("name");
         }};
